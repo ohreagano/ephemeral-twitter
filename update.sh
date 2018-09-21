f7f78a12d2af5659b01df9a009c2c2294558f908
Reagan Cline <reaganecline@gmail.com>
2018-09-21 18:34:55 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+
+GOOS=linux go build && \
+zip ephemeral.zip ephemeral && \
+rm ephemeral && \
+aws lambda update-function-code --function-name ephemeral --zip-file fileb://ephemeral.zip && \
+rm ephemeral.zip
