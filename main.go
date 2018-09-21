d2102884e7586f33542c966a6a21f1524630cca8
Reagan Cline <reaganecline@gmail.com>
2018-09-21 17:01:58 -0500

    Create main.go
    Copied from vickylai: https://github.com/vickylai/ephemeral/blob/master/main.go

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"net/url"
+	"os"
+	"time"
+
+	"log"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	consumerKey       = getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret    = getenv("TWITTER_CONSUMER_SECRET")
+	accessToken       = getenv("TWITTER_ACCESS_TOKEN")
+	accessTokenSecret = getenv("TWITTER_ACCESS_TOKEN_SECRET")
+	maxTweetAge       = getenv("MAX_TWEET_AGE")
+)
+
+func getenv(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		panic("missing required environment variable " + name)
+	}
+	return v
+}
+
+func getTimeline(api *anaconda.TwitterApi) ([]anaconda.Tweet, error) {
+	args := url.Values{}
+	args.Add("count", "200")        // Twitter only returns most recent 20 tweets by default, so override
+	args.Add("include_rts", "true") // When using count argument, RTs are excluded, so include them as recommended
+	timeline, err := api.GetUserTimeline(args)
+	if err != nil {
+		return make([]anaconda.Tweet, 0), err
+	}
+	return timeline, nil
+}
+
+func deleteFromTimeline(api *anaconda.TwitterApi, ageLimit time.Duration) {
+	timeline, err := getTimeline(api)
+
+	if err != nil {
+		log.Print("could not get timeline")
+	}
+	for _, t := range timeline {
+		createdTime, err := t.CreatedAtTime()
+		if err != nil {
+			log.Print("could not parse time ", err)
+		} else {
+			if time.Since(createdTime) > ageLimit {
+				_, err := api.DeleteTweet(t.Id, true)
+				log.Print("DELETED ID ", t.Id)
+				log.Print("TWEET ", createdTime, " - ", t.Text)
+				if err != nil {
+					log.Print("failed to delete: ", err)
+				}
+			}
+		}
+	}
+	log.Print("no more tweets to delete")
+
+}
+
+func ephemeral() {
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+	api.SetLogger(anaconda.BasicLogger)
+
+	h, _ := time.ParseDuration(maxTweetAge)
+
+	deleteFromTimeline(api, h)
+
+}
+
+func main() {
+
+	lambda.Start(ephemeral)
+
+}
